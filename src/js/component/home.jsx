ddc4d1e3abf91f704f2b80a1e358402c89b6c90b
Bradley Aikins <126637367+IssaPanda@users.noreply.github.com>
2024-09-30 17:55:56 +0000

    Glad I had this codespace saved for 3 weeks

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
-const Home = () => {
+function Home() {
+	const [input, setInput] = useState('');
+	const [chores, setChores] = useState([]);
+
+
+	const getTodos = () => {
+		fetch('https://playground.4geeks.com/todo/users/BradleyAikins')
+    .then(response => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    })
+    .then(responseAsJson => {
+        console.log(responseAsJson);
+
+		setChores(responseAsJson.todos)
+    })
+    .catch(error => {
+        console.log('Looks like there was a problem: \n', error);
+    });
+
+	}
+
+	React.useEffect(() => {getTodos()}, []);
+
+	const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setChores([...chores, [input]]);
+            setInput('');
+        }
+    };
+
+	const handleDelete = (indexToDelete) => {
+        setChores(chores.filter((_, index) => index !== indexToDelete));
+		
+    };
+	
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input type="text"
+				onChange={event => setInput(event.target.value)}
+				value={input}
+				onKeyDown={handleKeyDown} />
+			<ul>
+				{chores.map((chore, index) => (
+					<li key={index}>
+						{chore.label}
+						<button onClick={() => handleDelete(index.id)}>X</button>
+						</li>
+				))}
+			</ul>
 		</div>
 	);
-};
+}
 
 export default Home;
